41a1b7b4ff20d07874648ddabfede179210fff51
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-08 21:02:52 -0600

    Valid url

--- pokeapi-nodejs/src/lib/server.ts
+++ pokeapi-nodejs/src/lib/server.ts
@@ -3,9 +3,17 @@ import { createServer } from 'http';
 export default function server() {
     return createServer((req, res) => {
         res.setHeader("Content-Type", "application/json");
-    
-        if (req.url === "/" && req.method === "GET") {
+
+        const { url, method } = req;
+        
+        if (url === "/" && method === "GET") {
             res.end(JSON.stringify({ mensage: "Bienvenido a la POKEAPI de DiegoDev18" }));
+        }
+        
+        const regex = /^\/(\w+)\/*$/;
+        const validUrl = url?.match(regex);
+        if (!validUrl) {
+            res.end(JSON.stringify({ mensage: `URL Invalido! Solo se requiere un parametro. Ejemplo: /pikachu` }));
         }
     })
 }
